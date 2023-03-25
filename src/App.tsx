8c13ea0845934b6b8216c5c1fa7a44ccef7863a7
Shimi Zacken <shimizacken@gmail.com>
2023-03-25 13:47:29 +0100

    build: install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>Users</div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
