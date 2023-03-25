c0cb0d22c4133d0cfc02b4ba1133e6afcd2f3400
Shimi Zacken <shimizacken@gmail.com>
2023-03-25 14:51:22 +0100

    chore: text input stories added

--- src/packages/components/elements/TextInput/TextInput.stories.tsx
+++ src/packages/components/elements/TextInput/TextInput.stories.tsx
@@ -13,11 +13,17 @@ const Template: Story<TextInputProps> = args => <TextInput {...args} />;
 
 export const Text = Template.bind({});
 export const Password = Template.bind({});
+export const Placeholder = Template.bind({});
 
 Text.args = {
-    type: 'text',
+    value: 'Some text',
 };
 
 Password.args = {
     type: 'password',
+    value: '123456',
+};
+
+Placeholder.args = {
+    placeholder: 'Type user name to filter',
 };
--- src/packages/components/elements/TextInput/TextInput.view.tsx
+++ src/packages/components/elements/TextInput/TextInput.view.tsx
@@ -3,12 +3,18 @@ import styles from './TextInput.module.scss';
 
 export type TextInputType = 'text' | 'password';
 
-export const TextInput: React.FC<{type?: TextInputType; value?: string}> = ({
-    type = 'text',
-    value,
-    ...props
-}) => (
-    <input className={styles.textInput} type={type} value={value} {...props} />
+export const TextInput: React.FC<{
+    type?: TextInputType;
+    value?: string;
+    placeholder?: string;
+}> = ({type = 'text', placeholder = '', value, ...props}) => (
+    <input
+        className={styles.textInput}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        {...props}
+    />
 );
 
 export type TextInputProps = React.ComponentProps<typeof TextInput>;
