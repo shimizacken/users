62c6424db5af240076322c449b926fc92b83aa5d
Shimi Zacken <shimizacken@gmail.com>
2023-03-26 10:05:47 +0200

    chore: export components

---
+++ src/packages/components/index.ts
@@ -0,0 +1,6 @@
+// elements
+export {Card} from './elements/Card/Card.view';
+export {TextInput} from './elements/TextInput/TextInput.view';
+
+// modules
+export {ClickableCard} from './modules/ClickableCard/ClickableCard.view';
