d88f03ed9bcb26e506aad691e3686afee6a8c39c
Shimi Zacken <shimizacken@gmail.com>
2023-03-26 13:27:20 +0200

    style: text input

--- src/components/viewModels/UserCardsContainer.viewModel.tsx
+++ src/components/viewModels/UserCardsContainer.viewModel.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {useUsersLoader} from '../../hooks/useUsersLoader';
+import {TextInput} from '../../packages/components';
 import {usersService} from '../../services/usersService';
 import {UserCard} from '../views/UserCard/UserCard.view';
 import {UserCards} from '../views/UserCards/UserCards.view';
@@ -16,15 +17,20 @@ export const UserCardsContainer: React.FC = () => {
     }, [users]);
 
     return (
-        <UserCards>
-            {users?.map(user => (
-                <UserCard
-                    key={user.id}
-                    name={user.name}
-                    company={user.company.name}
-                    onClick={() => navigate(`user/${user.id}`)}
-                />
-            ))}
-        </UserCards>
+        <section>
+            <section>
+                <TextInput placeholder="Type user name..." />
+            </section>
+            <UserCards>
+                {users?.map(user => (
+                    <UserCard
+                        key={user.id}
+                        name={user.name}
+                        company={user.company.name}
+                        onClick={() => navigate(`user/${user.id}`)}
+                    />
+                ))}
+            </UserCards>
+        </section>
     );
 };
