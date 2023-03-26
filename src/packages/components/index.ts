45f45192d1e4b5b376b72f128e1ae9b6ff57a2c0
Shimi Zacken <shimizacken@gmail.com>
2023-03-26 10:38:08 +0200

    chore: foundations exported

--- src/packages/components/index.ts
+++ src/packages/components/index.ts
@@ -1,3 +1,7 @@
+// foundation
+export {Heading} from './foundation/Heading/Heading.view';
+export {Text} from './foundation/Text/Text.view';
+
 // elements
 export {Card} from './elements/Card/Card.view';
 export {TextInput} from './elements/TextInput/TextInput.view';
