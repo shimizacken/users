0483888c2974a44293cfa16fd25bf09ec23dd797
Shimi Zacken <shimizacken@gmail.com>
2023-03-26 10:38:16 +0200

    chore: classname added

--- src/packages/components/foundation/Heading/Heading.view.tsx
+++ src/packages/components/foundation/Heading/Heading.view.tsx
@@ -3,23 +3,23 @@ import React, {PropsWithChildren} from 'react';
 export type HeadingTag = 'h1' | 'h2' | 'h3' | 'h4';
 
 export const Heading: React.FC<
-    PropsWithChildren<{headingTag?: HeadingTag}>
-> = ({headingTag = 'h1', children}) => {
+    PropsWithChildren<{headingTag?: HeadingTag; className?: string}>
+> = ({headingTag = 'h1', className, children}) => {
     if (headingTag === 'h1') {
-        return <h1>{children}</h1>;
+        return <h1 className={className}>{children}</h1>;
     }
 
     if (headingTag === 'h2') {
-        return <h2>{children}</h2>;
+        return <h2 className={className}>{children}</h2>;
     }
 
     if (headingTag === 'h3') {
-        return <h3>{children}</h3>;
+        return <h3 className={className}>{children}</h3>;
     }
 
     if (headingTag === 'h4') {
-        return <h4>{children}</h4>;
+        return <h4 className={className}>{children}</h4>;
     }
 
-    return <h1>{children}</h1>;
+    return <h1 className={className}>{children}</h1>;
 };
